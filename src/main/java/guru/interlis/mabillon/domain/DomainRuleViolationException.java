06cd1bc6d7a35befbddc657dd4970c969dd2ba3b
Stefan Ziegler <2767800+edigonzales@users.noreply.github.com>
2026-08-17 17:32:57 +0000

    validation: classify domain rule violations as conflicts

--- src/main/java/guru/interlis/mabillon/domain/DomainRuleViolationException.java
+++ src/main/java/guru/interlis/mabillon/domain/DomainRuleViolationException.java
@@ -1,6 +1,6 @@
 package guru.interlis.mabillon.domain;
 
-public final class DomainRuleViolationException extends RuntimeException {
+public final class DomainRuleViolationException extends ConflictException {
 
     public DomainRuleViolationException(String message) {
         super(message);
