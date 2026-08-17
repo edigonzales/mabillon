baf3c42ae06f9591cad6027e8e6e0ecf45f0c13a
Stefan Ziegler <2767800+edigonzales@users.noreply.github.com>
2026-08-17 17:32:38 +0000

    validation: add not found exception

---
+++ src/main/java/guru/interlis/mabillon/domain/NotFoundException.java
@@ -0,0 +1,8 @@
+package guru.interlis.mabillon.domain;
+
+public final class NotFoundException extends MabillonException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
